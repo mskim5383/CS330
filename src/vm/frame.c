f233a2ec27a221cd446f7952e326778038ff57c2
Minsoo Kim <mskim5383@gmail.com>
2016-05-21 16:21:44 +0400

    added table_size variable instead of list_size (...)

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -12,8 +12,11 @@ void *find_entry_from_kpage (void *);
 struct list frame_table;
 struct lock frame_lock;
 struct lock frame_entry_lock;
+struct lock frame_size_lock;
+uint32_t table_size;
 
-#define FRAME_TABLE_SIZE 1024
+#define FRAME_TABLE_SIZE 300
+
 
 void
 frame_init (void)
@@ -21,20 +24,28 @@ frame_init (void)
   list_init (&frame_table);
   lock_init (&frame_lock);
   lock_init (&frame_entry_lock);
+  lock_init (&frame_size_lock);
+  table_size = 0;
 }
 
 struct frame_entry *
 frame_palloc (enum palloc_flags flags, void *pte)
 {
-  void *kpage;
+  void *kpage = NULL;
   struct frame_entry *f_e;
 
-  if (list_size (&frame_table) <= FRAME_TABLE_SIZE)
+  lock_acquire (&frame_size_lock);
+  if (table_size <= FRAME_TABLE_SIZE)
     kpage = palloc_get_page (flags);
+  table_size += 1;
+  lock_release (&frame_size_lock);
+
   if (kpage == NULL)
     kpage = swap_out ();
 
   f_e = (struct frame_entry *) malloc (sizeof (struct frame_entry));
+  ASSERT (f_e != NULL);
+
   lock_acquire (&frame_lock);
   f_e->pte = pte;
   f_e->kpage = kpage;
@@ -54,7 +65,11 @@ frame_free (void *kpage, bool palloc_free)
   lock_acquire (&frame_lock);
   f_e = find_entry_from_kpage (kpage);
   if (f_e == NULL)
-    PANIC ("frame_free: not allocated\n");
+  {
+    lock_release (&frame_lock);
+    return;
+  }
+
   *(f_e->pte) &= ~PTE_P;
   lock_acquire (&frame_entry_lock);
   list_remove (&f_e->elem);
@@ -62,7 +77,12 @@ frame_free (void *kpage, bool palloc_free)
   free (f_e);
   if (palloc_free)
     palloc_free_page (kpage);
+
   lock_release (&frame_lock);
+  
+  lock_acquire (&frame_size_lock);
+  table_size -= 1;
+  lock_release (&frame_size_lock);
 }
 
 struct frame_entry *
